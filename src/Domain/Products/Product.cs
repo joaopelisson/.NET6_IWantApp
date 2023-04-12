f5160f81ac95c91ca4dde8374b12637a97db5a04
joaopelisson <joaopedro.devbr@gmail.com>
2023-04-11 21:54:05 -0300

    Refactoring domain

--- src/Domain/Products/Product.cs
+++ src/Domain/Products/Product.cs
@@ -1,8 +1,7 @@
 ﻿namespace IWantApp.Domain.Products;
 
-public class Product
+public class Product : Entity
 {
-    public Guid Id { get; set; }
     public string Name { get; set; }
 
     public Category Category { get; set; }
@@ -10,12 +9,4 @@ public class Product
     public string Description { get; set; }
 
     public bool HasStock { get; set; }
-
-    public string CreatedBy { get; set; }
-
-    public DateTime CreatedOn { get; set; }
-
-    public string EditedBy { get; set; }
-
-    public DateTime EditedOn { get; set; }
 }
