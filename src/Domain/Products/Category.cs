c5def128cca5e7cf4ddbe430595f7c9eeedcc774
joaopelisson <joaopedro.devbr@gmail.com>
2023-04-11 21:44:30 -0300

    Creating product domain

---
+++ src/Domain/Products/Category.cs
@@ -0,0 +1,17 @@
+﻿namespace IWantApp.Domain.Products;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public string CreatedBy { get; set; }
+
+    public DateTime CreatedOn { get; set; }
+
+    public string EditedBy { get; set; }
+
+    public DateTime EditedOn { get; set;  }
+
+
+}
