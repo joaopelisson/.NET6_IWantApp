64f779f92856571b4e58db6589cf483b698cd263
joaopelisson <joaopedro.devbr@gmail.com>
2023-04-18 20:40:19 -0300

    Generating and applying migrations

--- src/Domain/Products/Product.cs
+++ src/Domain/Products/Product.cs
@@ -4,7 +4,7 @@ public class Product : Entity
 {
     public string Name { get; set; }
 
-    public int CategoryId { get; set }
+    public Guid CategoryId { get; set; }
 
     public Category Category { get; set; }
 
