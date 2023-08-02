24d6a6af8db2259f6e9d47a45f50118c0ba84959
joaopelisson <joaopedro.devbr@gmail.com>
2023-08-02 18:23:27 -0300

    feat: Saving authenticated user for category audits

--- src/Endpoints/Categories/CategoryPut.cs
+++ src/Endpoints/Categories/CategoryPut.cs
@@ -1,6 +1,7 @@
 ﻿using IWantApp.Domain.Products;
 using IWantApp.Infra.Data;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace IWantApp.Endpoints.Categories;
 
@@ -11,8 +12,9 @@ public class CategoryPut
 
     public static Delegate Handle => Action;
 
-    public static IResult Action([FromRoute]Guid id, CategoryRequest categoryRequest, ApplicationDbContext context)
+    public static IResult Action([FromRoute]Guid id, HttpContext http ,CategoryRequest categoryRequest, ApplicationDbContext context)
     {
+        var userId = http.User.Claims.First(c => c.Type == ClaimTypes.NameIdentifier).Value;
         var category = context.Categories.Where(c => c.Id == id).FirstOrDefault();
         
         if(category == null)
@@ -20,7 +22,7 @@ public class CategoryPut
             return Results.NotFound(); 
         }
 
-        category.EditInfo(categoryRequest.Name, categoryRequest.Active);
+        category.EditInfo(categoryRequest.Name, categoryRequest.Active, userId);
 
         if(!category.IsValid)
         {
